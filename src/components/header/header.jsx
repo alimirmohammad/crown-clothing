f34e33c7ee2676ad0e251f43c29bb1871ad0a403
alimirmohammad <ali.mirmohammad.86@gmail.com>
2020-04-18 22:11:24 +0430

    setup redux, added redux to App and Header

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -3,6 +3,7 @@ import './header.scss';
 import { Link } from 'react-router-dom';
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 import { auth } from '../../firebase/firebase.utils';
+import {connect} from 'react-redux';
 
 const Header = ({ currentUser }) => {
     return (
@@ -31,4 +32,8 @@ const Header = ({ currentUser }) => {
     )
 }
 
-export default Header;
+const mapStateToProps = state => ({
+    currentUser: state.user.currentUser
+});
+
+export default connect(mapStateToProps)(Header);
